ffbcf203bdd3d62b2724491d21c5cd1edcfb2e6a
harshpratik2411 <harshsrivastava2357@gmail.com>
2025-07-30 13:34:25 +0530

    short desc added to viewactivity

--- src/Pages/Activities/ViewActivity.jsx
+++ src/Pages/Activities/ViewActivity.jsx
@@ -60,18 +60,29 @@ const ViewActivities = () => {
           <div className="grid lg:grid-cols-2  gap-8 text-gray-700 text-base lg:text-lg mb-10">
             <div className="space-y-4 ">
               <div className="flex gap-3">
-                <span className="font-semibold lg:ml-0 ml-4 w-32">Activity Name:</span>
+                <span className="font-semibold lg:ml-0 ml-5 w-32">Activity Name:</span>
                 <span className='-ml-3'>{activities.name}</span>
+              </div>
+              <div className="flex gap-3">
+                <span className="font-semibold lg:ml-0 ml-5 w-32">Slug:</span>
+                <span className='-ml-3'>{activities.slug}</span>
               </div>
               
               <div className="flex lg:-ml-6 -ml-0 gap-3">
-                <span className="font-semibold lg:ml-6 ml-5 w-32">Booked:</span>
-                <span className='lg:-ml-3 -ml-5'>{activities.booked}</span>
+                <span className="font-semibold lg:ml-6 ml-5 w-32">Featured:</span>
+                <span className='mt-1'><input
+                      id="my-checkbox"
+                      type="checkbox"
+                      class="appearance-none w-4 h-4 border border-gray-400 rounded-md cursor-pointer
+                      checked:bg-primary checked:border-indigo-600 checked:text-white
+                      focus:ring-2 focus:ring-indigo-500  focus:ring-offset-2"
+                      />
+                      </span> 
               </div>
             </div>
 
-            <div className="flex items-start lg:items-center gap-3">
-              <span className="font-semibold lg:ml-0 ml-6 mt-1">Status:</span>
+            <div className="flex items-start lg:-mt-0 -mt-4 lg:items-center gap-3">
+              <span className="font-semibold lg:ml-0 ml-5 mt-1">Status:</span>
               <span
                 className={`px-4 py-2 lg:ml-6 ml-12 rounded-full text-sm font-medium ${getStatusClass(
                   activities.status
@@ -80,15 +91,17 @@ const ViewActivities = () => {
                 {activities.status}
               </span>
             </div>
-          </div>
+          <strong className='font-semibold lg:ml-0 ml-5 mt-1'>Short Description: </strong>
+          <span className='text-gray-600 font-medium lg:-mt-0 -mt-4 lg:ml-0 ml-5 text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eum animi illo exercitationem id et!</span>
+          </div>  
 
           {/* Static Description */}
           <div className="mt-6 border-t pt-6">
-            <div className="flex items-center gap-3 mb-2">
+            <div className="flex items-center  lg:ml-0 ml-5 gap-3 mb-2">
               <FaClipboardList className="text-lg text-gray-700" />
-              <h3 className="text-xl font-semibold text-gray-800">Activity Description</h3>
+              <h3 className="lg:text-xl text-lg font-semibold text-gray-800">Activity Description</h3>
             </div>
-            <p className="text-gray-600 text-left leading-relaxed">
+            <p className="text-gray-600 lg:ml-0 ml-5 text-left leading-relaxed">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu
               metus condimentum, finibus eros nec, volutpat odio. Integer et nisi a
               sem gravida sollicitudin. Suspendisse potenti. Sed cursus nunc sit amet
