ead1d26627944a44aa5ca544199c91822253095a
harshpratik2411 <harshsrivastava2357@gmail.com>
2025-08-06 17:42:54 +0530

    DifficultiesServices created

--- src/Pages/Difficulties/Difficulties.jsx
+++ src/Pages/Difficulties/Difficulties.jsx
@@ -1,25 +1,65 @@
 import React, { useEffect, useState } from "react";
 import AOS from "aos";
+import { format } from "date-fns";
 import "aos/dist/aos.css";
 import { BsThreeDotsVertical, BsXCircle } from "react-icons/bs";
 import { FaEye, FaEdit, FaPlus } from "react-icons/fa";
-import { FiSearch } from "react-icons/fi";
+import { FiSearch, FiFilter } from "react-icons/fi";
 import Navbar from "../../Components/Navbar/Navbar";
 import Sidebar from "../../Components/Siderbar/Sidebar";
 import { useNavigate } from "react-router-dom";
-import DifficultiesUI from "./DifficultiesUI";
+import DateFormatter from "../../Services/DateFormatter";
+import FilterOptions from "../../Services/FilterOptions";
+import StatusClassMap from "../../Services/StatusClassMap";
+import DifficultiesServices from "./DifficultiesServices"; 
+
+
 
 const Difficulties = () => {
   const [activeMenu, setActiveMenu] = useState(null);
   const [sortMenuOpen, setSortMenuOpen] = useState(false);
   const [searchText, setSearchText] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [difficultyList, setDifficultyList] = useState(DifficultiesUI);
-  const perPage = 5;
+ const [DifficultyList, setDifficultyList] = useState([]);
+  const [status, setStatus] = useState(""); // "Active" | "Inactive" | ""
+  const [orderBy, setOrderBy] = useState("");
+  const [selectedSortLabel, setSelectedSortLabel] = useState("Sort By");
+  const [loading, setLoading] = useState(true);
+  const [totalPages, setTotalPages] = useState(1); 
+
   const navigate = useNavigate();
+ 
+  
+
+  function apiCall(
+    search = searchText,
+    order = orderBy,
+    page = currentPage,
+    currentStatus = status
+  ) {
+    setLoading(true);
+
+    DifficultiesServices.getAll(search, order, page, currentStatus)
+      .then((response) => {
+       // console.log("🟩 Response from DifficultiesServices:", response);
+
+     setDifficultyList(response.difficulties);
+        setTotalPages(response.totalPages); 
+        setCurrentPage(response.currentPage);
+        setLoading(false);
+      })
+      .catch((err) => {
+       // console.error("🟥 API Call Error:", err);
+        setDifficultyList([]);
+        setTotalPages(1);
+        setLoading(false);
+      });
+  }
 
   useEffect(() => {
     AOS.init({ duration: 800 });
+
+    apiCall();
 
     const handleClickOutside = (event) => {
       if (
@@ -37,48 +77,34 @@ const Difficulties = () => {
 
   const toggleMenu = (id) => setActiveMenu(activeMenu === id ? null : id);
   const toggleSort = () => setSortMenuOpen(!sortMenuOpen);
+
   const handleSort = (option) => {
-    console.log("Sort by", option);
+    const newOrder = FilterOptions.filterMap[option];
+    setOrderBy(newOrder);
+    setSelectedSortLabel(option);
     setSortMenuOpen(false);
+    apiCall(searchText, newOrder, currentPage, status);
   };
 
-  const clearSearch = () => setSearchText("");
-
-  const filtered = difficultyList.filter((difficulty) =>
-    difficulty.name.toLowerCase().includes(searchText.toLowerCase())
-  );
-
-  const pages = Math.ceil(filtered.length / perPage);
-  const displayed = filtered.slice(
-    (currentPage - 1) * perPage,
-    currentPage * perPage
-  );
-
-  const updateBooked = (id, delta) => {
-    setDifficultyList((prev) =>
-      prev.map((difficulty) =>
-        difficulty.id === id
-          ? { ...difficulty, booked: Math.max(0, difficulty.booked + delta) }
-          : difficulty
-      )
-    );
+  const clearSearch = () => {
+    setSearchText("");
+    apiCall("", orderBy, currentPage, status);
   };
 
-  const getStatusClass = (status) => {
-    if (status === "Active") return "bg-blue-100 text-blue-600";
-    if (status === "InActive") return "bg-yellow-100 text-yellow-600";
-    return "bg-red-100 text-red-600";
-  };
+  const getStatusClass = (status) => StatusClassMap.getClass(status);
 
   return (
     <>
       <Navbar />
-      <Sidebar /> 
-        <h1 className='text-3xl font-bold -mt-10 text-center lg:ml-32 mb-10'>Difficulties</h1>
-      <div className="bg-white p-4 sm:p-6 lg:ml-72 rounded-xl shadow-md font-rubik w-full max-w-6xl mx-auto -mt-2">
-        {/* Search, Sort, Add */}
+      <Sidebar />
+      <h1 className="text-3xl font-bold -mt-10 text-center lg:ml-32 mb-10">
+        Difficulties
+      </h1>
+      <div className="bg-white  p-4 sm:p-6 lg:ml-72 rounded-xl shadow-md font-rubik w-full max-w-6xl mx-auto -mt-2 mb-12">
+        {/* Search, Sort, Filter, Add */}
         <div className="flex flex-col sm:flex-row sm:justify-between items-start sm:items-center gap-3 mb-4">
           <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 relative w-full sm:w-auto">
+            {/* Search Input */}
             <div className="relative w-full sm:w-auto">
               <FiSearch
                 className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-600"
@@ -87,7 +113,11 @@ const Difficulties = () => {
               <input
                 type="text"
                 value={searchText}
-                onChange={(e) => setSearchText(e.target.value)}
+                onChange={(e) => {
+                  const val = e.target.value;
+                  setSearchText(val);
+                  apiCall(val, orderBy, currentPage, status);
+                }}
                 placeholder="Search..."
                 className="border pl-10 pr-8 py-1 rounded text-sm focus:outline-none focus:ring w-full sm:w-52"
               />
@@ -101,22 +131,21 @@ const Difficulties = () => {
               )}
             </div>
 
+            {/* Sort Dropdown */}
             <div className="relative w-full sm:w-auto">
               <button
                 onClick={toggleSort}
                 className="sort-toggle flex items-center gap-1 border text-sm text-gray-600 bg-white px-3 py-1 rounded hover:bg-gray-100 w-full sm:w-auto"
               >
-                Sort By
+                {selectedSortLabel}
               </button>
               {sortMenuOpen && (
                 <div className="dropdown-menu absolute left-0 mt-1 bg-white border rounded shadow w-40 z-20">
                   {[
-                    "Name A‑Z",
-                    "Name Z‑A",
-                    "Date ASC",
-                    "Date DESC",
-                    "Popularity ASC",
-                    "Popularity DESC",
+                    FilterOptions.nameAsc,
+                    FilterOptions.nameDesc,
+                    FilterOptions.dateAsc,
+                    FilterOptions.dateDesc,
                   ].map((opt) => (
                     <button
                       key={opt}
@@ -129,108 +158,206 @@ const Difficulties = () => {
                 </div>
               )}
             </div>
+
+            {/* Status Filter */}
+            <select
+  value={status}
+  onChange={async (e) => {
+    const newStatus = e.target.value;
+    const newPage = 1;
+
+    const result = await apiCall(searchText, orderBy, newPage, newStatus);
+
+   
+    if (!result || result.length === 0) {
+      setCurrentPage(1); 
+    } else {
+      setCurrentPage(newPage); 
+    }
+
+    setStatus(newStatus); 
+  }}
+  className="border text-sm text-gray-600 bg-white px-3 py-1 rounded w-full sm:w-auto focus:outline-none"
+>
+  <option value="">All Status</option>
+  <option value="Active">Active</option>
+  <option value="Inactive">Inactive</option>
+</select>
           </div>
 
+          {/* Add Button */}
           <button
             onClick={() => navigate("/difficulties/add-difficulties")}
-            className="flex items-center gap-1 border text-sm text-gray-600 bg-white px-3 py-1 rounded hover:bg-gray-100 w-full sm:w-auto"
+            className="flex items-center gap-1 border text-sm text-gray-600 bg-white px-2 py-2 rounded hover:bg-gray-100"
           >
             <FaPlus size={12} /> Add
           </button>
         </div>
 
         {/* Table View */}
-        <div className="overflow-x-auto ">
-          <table className="min-w-full text-left text-sm">
-            <thead className="text-gray-500  font-rubik uppercase border-b">
-              <tr className="hidden sm:table-row">
-                <th className="lg:py-2">Name</th>
-                <th className="py-2">Status</th>
-                <th className="py-2">Description</th>
-                <th className="py-2">Slug</th>
-                <th className="py-2 ">Didfficulty Levels</th>
-                <th className="py-2"></th>
+        <div className="overflow-x-auto  hidden sm:block">
+          <table className="min-w-full  text-left text-sm">
+            <thead className="text-gray-500 font-rubik uppercase border-b">
+              <tr>
+                <th className="py-2  w-[15%]">Title</th>
+                <th className="py-2  w-[35%]">Description</th>
+                <th className="py-2  w-[10%]">Status</th>
+                <th className="py-2  w-[15%]">Created At</th>
+                <th className="py-2  w-[15%]">Updated At</th>
+                <th className="py-2  w-[10%] text-right">Actions</th>
               </tr>
             </thead>
             <tbody>
-              {displayed.map((difficulty) => (
-                <tr
-                  key={difficulty.id}
-                  data-aos="fade-up"
-                  className="border-b hover:bg-gray-50 relative  flex flex-col sm:table-row sm:flex-row"
-                >
-                  <td className="py-2 px-2  sm:px-0">
-                    <p className="font-medium lg:ml-0 ml-[2px] text-gray-800">
-                      {difficulty.name}
-                    </p>
-                  </td>
-
-                  <td className="py-2 px-2 sm:px-0">
-                    <span
-                      className={`px-3 py-1  rounded-full text-left text-xs font-medium ${getStatusClass(
-                        difficulty.status
-                      )}`}
-                    >
-                      {difficulty.status}
-                    </span>
-                  </td>
-
-                  <td className="py-2 px-2 lg:ml-0 ml-[2px] -mr-40  lg:block sm:px-0 text-gray-700 whitespace-normal break-words max-w-xs">
-                    {difficulty.desc}
-                  </td>
-                  <td className="py-2 px-2 lg:ml-0 ml-[2px] sm:px-0 text-gray-700">
-                    {difficulty.slug}
-                  </td>
-
-                  <td className="py-2 px-2 sm:px-0 text-gray-700">
-                    <span className="lg:ml-20 font-semibold">
-                      {difficulty?.level || "N/A"}
-                    </span>
-                  </td>
-
-                  <td className="py-2 px-2  sm:px-0 text-right sm:text-left">
-                    <div className="relative inline-block">
-                      <button
-                        onClick={() => toggleMenu(difficulty.id)}
-                        className="text-gray-600 hover:text-black menu-toggle"
+              {loading ? (
+                <tr>
+                  <td
+                    colSpan="6"
+                    className="text-center text-lg py-20 text-gray-500"
+                  >
+                    Loading...
+                  </td> 
+                </tr>
+              ) :
+               DifficultyList.length > 0 ? (
+               DifficultyList.map((Difficulty) => (
+                  <tr key={Difficulty.id} className="border-b  hover:bg-gray-50">
+                    <td className="py-6">
+                      <p className="font-medium text-gray-800">{Difficulty.name}</p>
+                    </td>
+                    <td className="py-4  text-gray-700">{Difficulty.desc}</td>
+                    <td className="py-4">
+                      <span
+                        className={`px-3 py-1 rounded-full text-xs font-medium ${getStatusClass(
+                          Difficulty.status
+                        )}`}
                       >
-                        <BsThreeDotsVertical className="" size={18} />
-                      </button>
-                      {activeMenu === difficulty.id && (
-                        <div className="dropdown-menu absolute right-0 -top-20 z-10 bg-white border rounded shadow w-32">
-                          <button
-                            onClick={() =>
-                              navigate(`/difficulties/view/${difficulty.id}`)
-                            }
-                            className="flex items-center gap-2 px-3 py-2 w-full hover:bg-gray-100 text-sm text-gray-700"
-                          >
-                            <FaEye size={14} /> View
-                          </button>
-                          <button
-                            onClick={() =>
-                              navigate(`/difficulties/update/${difficulty.id}`)
-                            }
-                            className="flex items-center gap-2 px-3 py-2 w-full hover:bg-gray-100 text-sm text-gray-700"
-                          >
-                            <FaEdit size={14} /> Update
-                          </button>
-                        </div>
-                      )}
-                    </div>
+                        {Difficulty.status}
+                      </span>
+                    </td>
+                    <td className="py-4 text-gray-700">
+                      {DateFormatter.formatDate(Difficulty.created_at)}
+                    </td>
+                    <td className="py-4 text-gray-700">
+                      {DateFormatter.formatDate(Difficulty.updated_at)}
+                    </td>
+                    <td className="py-4 text-right">
+                      <div className="relative inline-block">
+                        <button
+                          onClick={() => toggleMenu(Difficulty.id)}
+                          className="text-gray-600 hover:text-black menu-toggle"
+                        >
+                          <BsThreeDotsVertical size={18} />
+                        </button>
+                        {activeMenu === Difficulty.id && (
+                          <div className="dropdown-menu absolute right-0 -top-[4rem] z-10 bg-white border rounded shadow w-32">
+                            <button
+                              onClick={() => navigate(`/difficulties/view/${Difficulty.id}`)}
+                              className="flex items-center gap-2 px-3 py-2 w-full hover:bg-gray-100 text-sm text-gray-700"
+                            >
+                              <FaEye size={14} /> View
+                            </button>
+                            <button
+                              onClick={() => navigate(`/difficulties/update/${Difficulty.id}`)}
+                              className="flex items-center gap-2 px-3 py-2 w-full hover:bg-gray-100 text-sm text-gray-700"
+                            >
+                              <FaEdit size={14} /> Update
+                            </button>
+                          </div>
+                        )}
+                      </div>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td
+                    colSpan="6"
+                    className="text-center  text-3xl py-40 font-semibold text-gray-600"
+                  >
+                    No data found
                   </td>
                 </tr>
-              ))}
+              )}
             </tbody>
           </table>
         </div>
 
-        {/* Pagination */}
-        {pages > 1 && (
-          <div className="mt-6 flex justify-center items-center space-x-2">
-            {[...Array(pages)].map((_, idx) => (
+        {/* Mobile View */}
+        <div className="block sm:hidden space-y-5   flex-col items-center justify-center">
+          {!loading && DifficultyList && DifficultyList.length > 0 ? (
+            DifficultyList.map((difficulty) => (
+              <div
+                key={difficulty.id}
+                className="bg-gray-50 px-5 py-4 rounded-xl shadow-md border w-full"
+                data-aos="fade-up"
+              >
+                <div className="flex justify-between items-start">
+                  <div>
+                    <h2 className="text-lg font-semibold ml-1 text-gray-800 mb-2">
+                      {difficulty.name}
+                    </h2>
+                    <p
+                      className={`text-xs inline-block px-2 py-1 rounded ${getStatusClass(
+                        difficulty.status
+                      )}`}
+                    >
+                      {difficulty.status}
+                    </p>
+                  </div>
+                  <button
+                    onClick={() => toggleMenu(difficulty.id)}
+                    className="text-gray-600 menu-toggle"
+                  >
+                    <BsThreeDotsVertical size={20} />
+                  </button>
+                </div>
+                <div className="mt-4 ml-1 space-y-2 text-sm text-gray-700">
+                  <p>
+                    <span className="font-bold">Description:</span> {difficulty.desc}
+                  </p>
+                  <p>
+                    <span className="font-bold">Created At:</span>{" "}
+                    {DateFormatter.formatDate(difficulty.created_at)}
+                  </p>
+                  <p>
+                    <span className="font-bold">Updated At:</span>{" "}
+                    {DateFormatter.formatDate(difficulty.updated_at)}
+                  </p>
+                </div>
+                {activeMenu === difficulty.id && (
+                  <div className="mt-3 dropdown-menu bg-white border rounded shadow w-full z-10">
+                    <button
+                      onClick={() => navigate(`/difficulties/view/${tag.id}`)}
+                      className="flex items-center gap-2 px-3 py-2 w-full hover:bg-gray-100 text-sm text-gray-700"
+                    >
+                      <FaEye size={14} /> View
+                    </button>
+                    <button
+                      onClick={() => navigate(`/difficulties/update/${tag.id}`)}
+                      className="flex items-center gap-2 px-3 py-2 w-full hover:bg-gray-100 text-sm text-gray-700"
+                    >
+                      <FaEdit size={14} /> Update
+                    </button>
+                  </div>
+                )}
+              </div>
+            ))
+          ) : (
+            <div className="text-center font-semibold text-gray-600 py-10">
+              No data found
+            </div>
+          )}
+        </div>
+
+        {totalPages > 1 && (
+          <div className="mt-2 flex  justify-center items-center space-x-2">
+            {[...Array(totalPages)].map((_, idx) => (
               <button
                 key={idx + 1}
-                onClick={() => setCurrentPage(idx + 1)}
+                onClick={() => {
+                  apiCall(searchText, orderBy, idx + 1, status);
+                  setCurrentPage(idx + 1); // optional since apiCall sets it
+                }}
                 className={`px-3 py-1 rounded ${
                   currentPage === idx + 1
                     ? "bg-primary text-white"
@@ -246,9 +373,9 @@ const Difficulties = () => {
 
       {/* Floating Add Button on Mobile */}
       <button
-        onClick={() => navigate("/difficulties/add-difficulty")}
+        onClick={() => navigate("/difficulties/add-difficulties")}
         className="sm:hidden fixed bottom-3 right-6 bg-primary text-white p-4 rounded-full shadow-lg hover:bg-blue-700"
-        title="Add difficulty"
+        title="Add tag"
       >
         <FaPlus size={15} />
       </button>
---
+++ src/Pages/Difficulties/DifficultiesServices.jsx
@@ -0,0 +1,163 @@
+import APIService from "../APIServices";
+import LocalStorage from "../LocalStorage";
+import AuthService from "../AuthService";
+
+class DifficultiesServices {
+  static async getAll(search, orderBy, page, status) {
+    const url =
+      APIService.baseUrl +
+      `api/admin/difficulty/?search=${search}&ordering=${orderBy}&page=${page}&status=${status}`;
+
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("🟦 API RAW RESPONSE:", data);
+
+      return {
+        difficulties: data.results.map((difficulty) => ({
+          id: difficulty.id,
+          name: difficulty.title,
+          desc: difficulty.description,
+          status: difficulty.status,
+          created_at: difficulty.created_at,
+          updated_at: difficulty.updated_at,
+        })),
+        totalCount: data.count,
+        totalPages: Math.ceil(data.count / 10), 
+        currentPage: page,
+      };
+    } catch (error) {
+      console.error("Failed to fetch difficulties:", error);
+      return {
+        difficulties: [],
+        totalCount: 0,
+        totalPages: 1,
+        currentPage: 1,
+      };
+    }
+  }
+
+  static async get(id) {
+    const url = APIService.baseUrl + `api/admin/difficulty/${id}/`;
+    console.log("URL called", url);
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok)
+        throw new Error(`HTTP error! status: ${response.status}`);
+
+      const difficulty = await response.json();
+
+      return {
+        id: difficulty.id,
+        name: difficulty.title,
+        desc: difficulty.description,
+        status: difficulty.status,
+        created_at: difficulty.created_at,
+        updated_at: difficulty.updated_at,
+      };
+    } catch (error) {
+      console.error("Failed to fetch difficulty:", error);
+      return null;
+    }
+  }
+
+  // Add a new difficulty
+  static async add(data) {
+    const url = APIService.baseUrl + "api/admin/difficulty/";
+    console.log("Data = ", data);
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: LocalStorage.getAccesToken(),
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (APIService.isUnauthenticated(response.status)) {
+        await APIService.refreshToken();
+        return this.add(data); // Make sure to return recursive call
+      }
+
+      if (APIService.isError(response.status)) {
+        const errorData = await response.json();
+        alert(errorData["error"]);
+        return null;
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error("Failed to add difficulty:", error);
+      return null;
+    }
+  }
+
+  // Update difficulty by ID
+  static async update(id, data) {
+    console.log("Update API called");
+
+    const url = APIService.baseUrl + `api/admin/difficulty/${id}/`;
+
+    try {
+      let response = await fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: LocalStorage.getAccesToken(),
+        },
+        body: JSON.stringify(data),
+      });
+
+      console.log("Response = ", response.status);
+
+      if (APIService.isUnauthenticated(response.status)) {
+        await APIService.refreshToken();
+        return this.update(id, data); // Return recursive call
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error(`Failed to update difficulty with id ${id}:`, error);
+      return null;
+    }
+  }
+
+  // Delete difficulty by ID
+  static async delete(id) {
+    const url = APIService.baseUrl + `api/admin/difficulty/${id}/`;
+
+    try {
+      let response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: LocalStorage.getAccesToken(),
+        },
+      });
+
+      console.log("Response = ", response.status);
+
+      if (APIService.isUnauthenticated(response.status)) {
+        await APIService.refreshToken();
+        return this.delete(id); // Return recursive call
+      } else if (APIService.isDeleted(response.status)) {
+        return true;
+      }
+
+      return false;
+    } catch (error) {
+      console.error(`Failed to delete difficulty with id ${id}:`, error);
+      return false;
+    }
+  }
+}
+
+export default DifficultiesServices;
